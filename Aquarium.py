6a1e58b008e02d581f9d301d3da075579d3c5e27
mekdeskebede <kebedemekdes289@gmail.com>
2022-06-13 00:13:24 +0300

    grass

--- Aquarium.py
+++ Aquarium.py
@@ -96,22 +96,22 @@ def draw():
 
         glBegin(GL_POLYGON)
         glColor3f(0.0, 1.0, 0.0)
-        glVertex3f(0.4, -0.4, 0.0)
+        # glVertex3f(0.4, -0.4, 0.0)
 
-        glVertex3f(0.35, 0.75, 0.0)
-        glVertex3f(0.2, 0.35, 0.0)
+        # glVertex3f(0.35, 0.75, 0.0)
+        # glVertex3f(0.2, 0.35, 0.0)
         glEnd()
 
         glBegin(GL_LINES)
-        glColor3f(1.0, 1.0, 1.0)
-        glVertex3f(0.35, 0.75, 0.0)
-        glVertex3f(0.4, -0.4, 0.0)
+        # glColor3f(1.0, 1.0, 1.0)
+        # glVertex3f(0.35, 0.75, 0.0)
+        # glVertex3f(0.4, -0.4, 0.0)
         glEnd()
         glBegin(GL_POLYGON)
-        glColor3f(0.0, 1.0, 0.0)
-        glVertex3f(0.4, -0.4, 0.0)
-        glVertex3f(0.5, 0.35, 0.0)
-        glVertex3f(0.35, 0.75, 0.0)
+        # glColor3f(0.0, 1.0, 0.0)
+        # glVertex3f(0.4, -0.4, 0.0)
+        # glVertex3f(0.5, 0.35, 0.0)
+        # glVertex3f(0.35, 0.75, 0.0)
         glEnd()
         glPopMatrix()
     glPopMatrix()
@@ -254,6 +254,43 @@ def draw():
     glEnd()
 
     glPopMatrix()
+
+
+
+    for i in range(100):
+        i = i/5
+        glColor3f(0.0,1.0,0.0)
+        glLineWidth(2.0)
+        glBegin(GL_LINES)
+        glVertex2f(-0.9+i,-0.85)
+        glVertex2f(-0.8+i,-0.95)
+        glVertex2f(-0.8+i,-0.85)
+        glVertex2f(-0.8+i,-0.95)
+        glVertex2f(-0.7+i,-0.85)
+        glVertex2f(-0.8+i,-0.95)
+        glVertex2f(-0.75+i,-0.85)
+        glVertex2f(-0.8+i,-0.95)
+        glVertex2f(-0.85+i,-0.85)
+        glVertex2f(-0.8+i,-0.95)
+        glVertex2f(-0.725+i,-0.85)
+        glVertex2f(-0.8+i,-0.95)
+        glVertex2f(-0.775+i,-0.85)
+        glVertex2f(-0.8+i,-0.95)
+        glVertex2f(-0.825+i,-0.85)
+        glVertex2f(-0.8+i,-0.95)
+        glVertex2f(-0.875+i,-0.85)
+        glVertex2f(-0.8+i,-0.95)
+        glEnd()
+
+    glBegin(GL_POLYGON)
+    glVertex2f(-1,-0.96)
+    glVertex2f(-1,-1)
+    glVertex2f(1,-1)
+    glVertex2f(1,-0.96)
+    glEnd()
+
+
+
 
     # glutSwapBuffers()
 
