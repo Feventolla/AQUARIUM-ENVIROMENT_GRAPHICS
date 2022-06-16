abc10886dc874b3ea08f0916350403a0343b5fe1
fev <feventolla99@gmail.com>
2022-06-16 11:40:29 +0300

    floor decor

--- Aquarium.py
+++ Aquarium.py
@@ -13,11 +13,13 @@ bubblepathx = -0.5
 bubblepathy = -0.5
 largebubblepathx = -0.5
 largebubblepathy = -0.5
-fish1path = 0.8
 
+fish1path = 0.8
+fish3pathy = 0.8
+fish2pathy = -0.2
 
 fishX = 4.0
-fishY= 3.0
+fishY = 3.0
 
 points = [-0.1, -0.19, -0.28, -0.37, -0.46, -
           0.55, -0.64, -0.73, -0.82, -0.91, -1.0]
@@ -28,21 +30,15 @@ onlyPoints = [-0.1, -0.24, -0.41, -0.56]
 
 points = [-0.1, -0.22, -0.34, -0.45, -0.56, -
           0.65, -0.74, -0.83, -0.92, -1.0]
-<<<<<<< HEAD
-value = [-3.6, -3.6,  -2.4, -1.2
-         #  -1.8, -1.2, -0.8, 0.0, 0.4,
-         #          0.8,  1.2,  1.6, 2.0, 2.4, 2.8, 3.2, 3.6
-         ]
-value1 = [-3.6,   -2.4, -1.2, 0.3
-          # -0.6, -0.2, 0.4,
-          #           0.8,  1.2,  1.6, 2.0, 2.4, 2.8, 3.2, 3.6
-          ]
-=======
 
-value = [-3.6, -3.4, -3.2, - 3.0, -2.8, -2.6, -2.4, -
-         2.2, -2.0, -1.8, -1.6, -1.4, -1.2, -1.0, -0.8, -0.6, 2.8, 2.6, 2.4,
-         2.2, 2.0, 1.8, 1.6, 1.4, 1.2, 1.0, 0.8, 0.6]
->>>>>>> 92b88ef312592cb850426482ad78d2cdcc7e66ae
+value = [-3.6,  -3.2, -2.8, -2.4, -2.0,
+          -1.6,  -1.2,  -0.8, -0.4, 0.0,
+         0.4, 3.2, 3.6, 2.8,  2.4,
+         2.0,  1.6,  1.2,  0.8
+         ]
+# value1 = [-3.2,  -2.8, -2.4, -
+#           -2.0, -1.6,  -1.2,  -0.8,  2.8,  2.4,
+#           2.0,  1.6,  1.2,  0.8, ]
 
 
 def init():
@@ -53,7 +49,7 @@ def init():
 
 
 def move_time(key):
-    global bubblepathx, largebubblepathx, bubblepathy, largebubblepathy, fish1path
+    global bubblepathx, largebubblepathx, bubblepathy, largebubblepathy, fish1path, fish3pathy, fish2pathy
     if(bubblepathx > 2.0):
         bubblepathx = -0.6
     bubblepathx += 0.03
@@ -73,6 +69,14 @@ def move_time(key):
     if(fish1path < -2):
         fish1path = 0.8
     fish1path -= 0.008
+
+    if(fish3pathy < -2):
+        fish3pathy = 0.8
+    fish3pathy -= 0.008
+
+    if(fish2pathy > 2):
+        fish2pathy = -0.2
+    fish2pathy += 0.004
 
     glutPostRedisplay()
     glutTimerFunc(10, move_time, 0)
@@ -203,122 +207,110 @@ def fish1():
 def fish2():
     glPushMatrix()
     glScalef(1.0, 1.0, 0.0)
-    glTranslated(-0.4, 0.0, 0.0)
-    glColor3f(0.7, 0.25, 0.20) 
+    glTranslated(-fish1path, fish2pathy, 0.0)
+    glColor3f(0.7, 0.25, 0.20)
     glBegin(GL_POLYGON)
     glVertex2f(-0.2, -0.6)
     glVertex2f(0.2, -0.5)
     glVertex2f(0.2, -0.7)
-    glColor4f(0.1, 0.25, 0.20,0.5)
+    glColor4f(0.1, 0.25, 0.20, 0.5)
     glVertex2f(0.5, -0.6)
     glVertex2f(0.2, -0.5)
 
     glEnd()
     glBegin(GL_TRIANGLES)
-    glColor3f(0.7, 0.25, 0.20) 
+    glColor3f(0.7, 0.25, 0.20)
     glVertex2f(0.2, -0.5)
     glVertex2f(0.17, -0.55)
-    glColor4f(0.1, 0.25, 0.20,0.5)
+    glColor4f(0.1, 0.25, 0.20, 0.5)
     glVertex2f(0.1, -0.45)
     glVertex2f(0.2, -0.7)
-    glColor3f(0.7, 0.25, 0.20) 
+    glColor3f(0.7, 0.25, 0.20)
     glVertex2f(0.17, -0.65)
     glVertex2f(0.1, -0.75)
 
-
     glVertex2f(-0.18, -0.6)
     glVertex2f(-0.27, -0.6)
-    glColor4f(0.1, 0.25, 0.20,0.5)
+    glColor4f(0.1, 0.25, 0.20, 0.5)
     glVertex2f(-0.3, -0.67)
     glColor3f(0.7, 0.25, 0.20)
     glVertex2f(-0.18, -0.6)
     glVertex2f(-0.27, -0.6)
-    glColor4f(0.1, 0.25, 0.20,0.5)
+    glColor4f(0.1, 0.25, 0.20, 0.5)
     glVertex2f(-0.3, -0.53)
 
-    
     glEnd()
     glColor3f(1, 1, 1)
     glPointSize(3)
 
-    
-
-
     glBegin(GL_POINTS)
     glVertex2f(0.4, -0.6)
     glEnd()
-   
 
     glPopMatrix()
     glFlush()
 
+
 def fish3():
     glScalef(0.6, 0.6, 0.0)
-    # glTranslatef(0,0, 0.0) 
-    
-    glColor4f(0.7, 0.4, 0.0,0.0)
+    glTranslated(fish1path+0.01, fish3pathy, 0.0)
+    glColor4f(0.7, 0.4, 0.0, 0.0)
     glBegin(GL_TRIANGLES)
-    
-    glVertex2f(0.428, 0.227)
-    glVertex2f(0.51, 0.1) 
-    glVertex2f(0.428,0.335)
 
-    
+    glVertex2f(0.428, 0.227)
+    glVertex2f(0.51, 0.1)
+    glVertex2f(0.428, 0.335)
+
     glVertex2f(0.429, 0.343)
     glVertex2f(0.51, 0.512)
     glVertex2f(0.429, 0.20)
-   
+
     glEnd()
     glBegin(GL_POLYGON)
     glVertex3f(0.429, 0.343, 0.0)
     glVertex3f(0.306, 0.376, 0.0)
     glVertex3f(0.292, 0.401, 0.0)
-    glColor4f(1.0, 0.5, 0.0,0.0)
+    glColor4f(1.0, 0.5, 0.0, 0.0)
     glVertex3f(0.226, 0.416, 0.0)
     glVertex3f(0.200, 0.419, 0.0)
-    glColor4f(1,1,1,0)
+    glColor4f(1, 1, 1, 0)
     glVertex3f(0.164, 0.409, 0.0)
     glVertex3f(0.117, 0.388, 0.0)
-    glColor4f(1.0, 0.5, 0.0,0.0)
+    glColor4f(1.0, 0.5, 0.0, 0.0)
     glVertex3f(0.077, 0.356, 0.0)
     glVertex3f(0.052, 0.322, 0.0)
-    glColor4f(1,1,1,0)
+    glColor4f(1, 1, 1, 0)
     glVertex3f(0.038, 0.287, 0.0)
     glVertex3f(0.041, 0.244, 0.0)
-    glColor4f(1.0, 0.5, 0.0,0.0)
+    glColor4f(1.0, 0.5, 0.0, 0.0)
     glVertex3f(0.061, 0.219, 0.0)
     glVertex3f(0.108, 0.183, 0.0)
-    glColor4f(1,1,1,0)
+    glColor4f(1, 1, 1, 0)
     glVertex3f(0.168, 0.160, 0.0)
     glVertex3f(0.204, 0.153, 0.0)
-    glColor4f(1.0, 0.5, 0.0,0.0)
+    glColor4f(1.0, 0.5, 0.0, 0.0)
     glVertex3f(0.231, 0.154, 0.0)
     glVertex3f(0.288, 0.167, 0.0)
-    glColor4f(1,1,1,0)
+    glColor4f(1, 1, 1, 0)
     glVertex3f(0.340, 0.187, 0.0)
     glVertex3f(0.403, 0.215, 0.0)
-    glColor4f(1.0, 0.5, 0.0,0.0)
+    glColor4f(1.0, 0.5, 0.0, 0.0)
     glVertex3f(0.428, 0.227, 0.0)
     glEnd()
 
     glPointSize(3)
     glBegin(GL_POINTS)
-  
-    glColor3f(0.1,0.1,0.3)
-    
-    glVertex2f(0.09,0.3)
+
+    glColor3f(0.1, 0.1, 0.3)
+
+    glVertex2f(0.09, 0.3)
     glEnd()
-    glColor4f(0.7, 0.4, 0.0,0.0)
+    glColor4f(0.7, 0.4, 0.0, 0.0)
     glBegin(GL_TRIANGLES)
-    glVertex2f(0.12,0.340)
-    glVertex2f(0.20,0.340)
-    glVertex2f(0.22,0.27)
+    glVertex2f(0.12, 0.340)
+    glVertex2f(0.20, 0.340)
+    glVertex2f(0.22, 0.27)
     glEnd()
-    
-    
-
-
-
 
 
 def draw():
@@ -347,17 +339,33 @@ def draw():
     # glVertex2f(-1.0, 1.0)
     # glEnd()
     #full sky blue ###
+    for k in value:
+        glPushMatrix()
+        glScalef(0.3, 0.3, 0.0)
+        glTranslated(k, -3.0, 0.0)
+        posx, posy = 0, 0
+        sides = 32
+        radius = 0.06
+        glBegin(GL_POLYGON)
+        glColor3f(1.0, 0.36, 0.25)
+        for i in range(100):
+            cosine = radius * cos(i*2*pi/sides)
+            sine = radius * sin(i*2*pi/sides)
+
+            glVertex2f(sine, cosine)
+        glEnd()
+        glPopMatrix()
 
     for k in value:
         glPushMatrix()
         glScalef(0.3, 0.3, 0.0)
         glTranslated(k, -4.5, 0.0)
         posx, posy = 0, 0
-        sides = 65
+        sides = 32
         radius = 1.5
         glBegin(GL_POLYGON)
 
-        glColor3f(0.0, 1.0, 0.0)
+        glColor3f(0.0, 0.54, 0.0)
         for i in range(30):
             cosine = radius * cos(i/sides)
             sine = radius * sin(i/sides)
@@ -365,20 +373,37 @@ def draw():
             glVertex2f(sine, cosine)
         glEnd()
         glPopMatrix()
-    for k in value1:
+    
+    for k in value:
         glPushMatrix()
         glScalef(0.3, 0.3, 0.0)
-        glTranslated(k, -4.5, 0.0)
+        glTranslated(k+0.05, -4.7, 0.0)
         posx, posy = 0, 0
-        sides = 65
+        sides = 32
         radius = 1.5
         glBegin(GL_POLYGON)
-        glColor3f(0.0, 1.0, 0.0)
+        glColor3f(0.0, 0.54, 0.0)
         for i in range(30):
             cosine = radius * cos(i/sides)
             sine = radius * sin(i/sides)
 
             glVertex2f(-sine, cosine)
+        glEnd()
+        glPopMatrix()
+    for k in value:
+        glPushMatrix()
+        glScalef(0.3, 0.3, 0.0)
+        glTranslated(k, -3.25, 0.0)
+        posx, posy = 0, 0
+        sides = 32
+        radius = 0.05
+        glBegin(GL_POLYGON)
+        glColor3f(0.95, 0.36, 0.25)
+        for i in range(100):
+            cosine = radius * cos(i*2*pi/sides)
+            sine = radius * sin(i*2*pi/sides)
+
+            glVertex2f(sine, cosine)
         glEnd()
         glPopMatrix()
     # glPushMatrix()
@@ -444,7 +469,7 @@ def draw():
     # dimond
     glPushMatrix()
     glColor3f(0.7255, 0.949, 1.0)
-    glTranslated(-1.5, -0.5, 0.0)
+    glTranslated(-0.85, -0.5, 0.0)
     glBegin(GL_POLYGON)
     glVertex2f(1.0, -0.125)
     glVertex2f(0.92, -0.125)
@@ -457,7 +482,7 @@ def draw():
     glPopMatrix()
 
     glPushMatrix()
-    glTranslated(-1.5, -0.5, 0.0)
+    glTranslated(-0.85, -0.5, 0.0)
     glColor3f(1.0, 1.0, 1.0)
     glBegin(GL_LINE_STRIP)
     glVertex2f(1.05, -0.17)
@@ -469,38 +494,6 @@ def draw():
     glPopMatrix()
 
 
-def grass():
-    for i in range(100):
-        i = i/5
-        glColor3f(0.0, 1.0, 0.0)
-        glLineWidth(2.0)
-        glBegin(GL_LINES)
-        glVertex2f(-0.9+i, -0.85)
-        glVertex2f(-0.8+i, -0.95)
-        glVertex2f(-0.8+i, -0.85)
-        glVertex2f(-0.8+i, -0.95)
-        glVertex2f(-0.7+i, -0.85)
-        glVertex2f(-0.8+i, -0.95)
-        glVertex2f(-0.75+i, -0.85)
-        glVertex2f(-0.8+i, -0.95)
-        glVertex2f(-0.85+i, -0.85)
-        glVertex2f(-0.8+i, -0.95)
-        glVertex2f(-0.725+i, -0.85)
-        glVertex2f(-0.8+i, -0.95)
-        glVertex2f(-0.775+i, -0.85)
-        glVertex2f(-0.8+i, -0.95)
-        glVertex2f(-0.825+i, -0.85)
-        glVertex2f(-0.8+i, -0.95)
-        glVertex2f(-0.875+i, -0.85)
-        glVertex2f(-0.8+i, -0.95)
-        glEnd()
-
-    glBegin(GL_POLYGON)
-    glVertex2f(-1, -0.96)
-    glVertex2f(-1, -1)
-    glVertex2f(1, -1)
-    glVertex2f(1, -0.96)
-    glEnd()
 
 
 def display():
@@ -525,9 +518,7 @@ def display():
     circle_bubbles()
     glPopMatrix()
 
-    glPushMatrix()
-    grass()
-    glPopMatrix()
+    
 
     # glutSwapBuffers()
 
